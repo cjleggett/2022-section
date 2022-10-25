7bda7b0a43325b71efef0253a3ab60063473f643
Connor Leggett <cleggett@Connors-MacBook-Pro.local>
2022-10-24 23:08:41 -0400

    week 7

---
+++ section7/outline/practice_queries.sql
@@ -0,0 +1,23 @@
+-- What is Kalos's student id?
+
+
+-- What is the course title for CS51?
+
+
+-- What are the course codes and titles for all of the CS courses?
+-- (Assume that all CS courses have a course code that begins with 'CS')
+
+
+-- How many courses are there?
+
+
+-- How many students are taking CS50?
+
+
+-- What are the names of all of the instructors? Generate a table with all instructors' names and the course they teach.
+
+-- What are the names of all of the students taking CS50?
+
+-- What is the average score of students in STAT 110?
+
+-- What are sophie's scores in all of their classes?
