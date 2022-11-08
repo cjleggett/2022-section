6093758a294757d8ec57d59b6ef92cb5c784391b
Connor Leggett <43161915+cjleggett@users.noreply.github.com>
2022-11-08 17:41:15 -0500

    after section 9 (#12)

--- section9/solutions/add_tables.sql
+++ section9/solutions/add_tables.sql
@@ -1,26 +1,4 @@
--- people
-CREATE TABLE people (
-  id INTEGER PRIMARY KEY AUTOINCREMENT,
+-- Code written for creating the users table
+CREATE TABLE users (
   name TEXT NOT NULL,
-  senior INTEGER
-);
-
--- courses
-CREATE TABLE courses (
-  id INTEGER PRIMARY KEY AUTOINCREMENT,
-  code TEXT NOT NULL,
-  title TEXT NOT NULL
-);
-
--- students
-CREATE TABLE students (
-  person_id INTEGER NOT NULL,
-  course_id INTEGER NOT NULL,
-  score REAL
-);
-
--- instructors
-CREATE TABLE instructors (
-  person_id INTEGER NOT NULL,
-  course_id INTEGER NOT NULL
 );
