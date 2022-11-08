8fd6ec90972b94054874dceee09e9a616ea3fbc2
Connor Leggett <cleggett@Connors-MacBook-Pro.local>
2022-11-08 17:40:35 -0500

    after section 9

--- section9/solutions/app.py
+++ section9/solutions/app.py
@@ -1,8 +1,45 @@
 from flask import Flask, render_template, request
+from cs50 import SQL
+
+db = SQL("sqlite:///users.db")
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def index():
-    return "Hello, World!"
+    return render_template("index.html")
+
+@app.route("/cat")
+def cat():
+    return "You are on the Cat Page!"
+
+@app.route("/welcome/<name>")
+def welcome(name):
+    db.execute("INSERT INTO users VALUES (?)", name)
+    users = db.execute("SELECT * FROM users")
+    # The below line uses Pthon's list comprehension, but it is the equivalen of writing:
+    # temp_users = []
+    # for row in users:
+    #     temp_users.append(row["name"])
+    # users = temp_users
+    users = [row["name"] for row in users]
+    return render_template(
+        "welcome.html",
+        x=name,
+        users=users
+    )
+
+@app.route("/welcome2")
+def welcome2():
+    name = request.args.get("q")
+    return f"Welcome, {name}"
+
+@app.route("/survey", methods=["GET", "POST"])
+def survey():
+    if request.method == "POST":
+        mood = request.form.get("mood")
+        age = request.form.get("age")
+        return f"Your mood is {mood} and you are {age} years old."
+    else:
+        return render_template("survey.html")
